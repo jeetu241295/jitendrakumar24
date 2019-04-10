75d2eefc0b5771cb17848167ef3f53364d2c7714
Jitendra Kumar <jeetu241295@gmail.com>
2019-04-10 10:48:43 +0530

    Tab Page

---
+++ src/Components/Tabs.jsx
@@ -0,0 +1,76 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles, Grid } from '@material-ui/core';
+import SwipeableViews from 'react-swipeable-views';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+
+const styles = theme => ({
+  root: {
+    backgroundColor: theme.palette.background.paper,
+  },
+  tabContent: {
+    padding: '2rem',
+  },
+});
+
+class FullWidthTabs extends React.Component {
+  state = {
+    value: 0,
+  };
+
+  handleChange = (event, value) => {
+    this.setState({ value });
+  };
+
+  handleChangeIndex = index => {
+    this.setState({ value: index });
+  };
+
+  render() {
+    const { classes, theme, tabs } = this.props;
+    const { value } = this.state;
+    return (
+      <div className={classes.root}>
+        <AppBar position="static" color="default">
+          <Tabs
+            value={value}
+            onChange={this.handleChange}
+            indicatorColor="primary"
+            textColor="primary"
+            variant="fullWidth"
+          >
+            {tabs.map(item => (
+              <Tab key={item.id} label={item.label} />
+            ))}
+          </Tabs>
+        </AppBar>
+        <SwipeableViews
+          axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
+          index={value}
+          onChangeIndex={this.handleChangeIndex}
+        >
+          {tabs.map(item => (
+            <Grid
+              className={classes.tabContent}
+              key={item.id}
+              dir={theme.direction}
+              container
+            >
+              {item.content}
+            </Grid>
+          ))}
+        </SwipeableViews>
+      </div>
+    );
+  }
+}
+
+FullWidthTabs.propTypes = {
+  classes: PropTypes.object.isRequired,
+  theme: PropTypes.object.isRequired,
+  tabs: PropTypes.array.isRequired,
+};
+
+export default withStyles(styles, { withTheme: true })(FullWidthTabs);
