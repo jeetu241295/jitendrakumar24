f5a55ba8a02cd6a6daadd520b131e8e87922907e
Jitendra Kumar <jeetu241295@gmail.com>
2021-01-28 23:41:21 +0530

    dev tools change

--- src/index.js
+++ src/index.js
@@ -10,10 +10,12 @@ import PersonalWebsite from './Widget/components/App';
 import theme from './Global/theme';
 
 const composeEnhancers =
-  process.env.NODE_ENV === 'production' &&
+  process.env.NODE_ENV === 'development' &&
   window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
     ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__()
     : compose;
+
+console.log(process.env.NODE_ENV === 'development', composeEnhancers);
 
 const store = createStore(
   rootReducer,
